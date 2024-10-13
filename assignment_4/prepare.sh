ae18dac2e7e51481758309a4b07a93222718dedb
danielmoreira <7726831+danielmoreira@users.noreply.github.com>
2024-10-13 00:46:12 -0500

    Added the content of the 4th assignment.

--- assignment_4/prepare.sh
+++ assignment_4/prepare.sh
@@ -13,4 +13,4 @@ mkdir -p $HOME/comp141_assign4
 rm -rf $HOME/comp141_assign4/*
 cp ./script/vir.sh $HOME/comp141_assign4/vir.sh
 chmod a-x $HOME/comp141_assign4/vir.sh
-echo "Program successfully copied to $HOME/comp141_assign4/vir.sh."
+echo "Program successfully copied to $HOME/comp141_assign4/"
